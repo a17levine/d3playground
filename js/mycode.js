fd67a7fe939d5b79150088b4d0ccfeaf52eb0f68
Alex Levine <a17levine@gmail.com>
2014-11-05 09:30:41 -0600

    completed ex 4 - the map

--- js/mycode.js
+++ js/mycode.js
@@ -1,31 +1,29 @@
-// var svg = d3.select('.stage')
-//   .append('svg')        // create an <svg> element
-//     .attr('width', 300) // set its dimensions
-//     .attr('height', 150);
+var lifeExpectancies = {"AL":75.4, "AK":78.3, "AZ":79.6, "AR":76, "CA":80.8, "CO":80, "CT":80.8, "DE":78.4, "DC":76.5, "FL":79.4, "GA":77.2, "HI":81.48, "ID":79.5, "IL":79, "IN":77.6, "IA":79.7, "KS":78.7, "KY":76, "LS":75.7, "ME":79.2, "MD":78.8, "MA":80.5, "MI":78.2, "MN":80.85, "MS":75, "MO":77.5, "MT":78.5, "NE":79.8, "NV":78.1, "NH":80.3, "NJ":80.3, "NM":78.4, "NY":80.5, "NC":77.8, "ND":79.5, "OH":77.8, "OK":75.9, "OR":79.5, "PA":78.5, "RI":79.9, "SC":77, "SD":79.5, "TN":76.3, "TX":78.5, "UT":80.2, "VT":80.5, "VA":79, "WA":79.9, "WV":75.4, "WI":80, "WY":78.3};
 
-// var data = [{x:10, y: 10}, {x:40, y: 20}];
+var scale = d3.scale.linear()
+                    .domain([75, 81.48])
+                    .range([0, 1]);
 
-// var circle = svg.selectAll("circle")
-//     .data(data);
-    
-// circle.enter().append("circle");
+function tooltipHtml(n, d) { /* function to create html content string in tooltip div. */
+    return "<h4>" + n + "</h4><table>" +
+        "<tr><td>Life expectancy</td><td>" + (d.years) + " years</td></tr>" +
+        "</table>";
+}
 
-// circle
-//     .attr("r", 10)
-//     .attr("cx", function(d) { return d.x;})
-//     .attr("cy", function(d) { return d.y;});
-    
-// circle.exit().remove();
+var sampleData = {}; /* Sample random data. */
+["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+    "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
+    "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
+    "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
+    "WI", "MO", "AR", "OK", "KS", "LS", "VA"
+]
+.forEach(function(d) {
+    sampleData[d] = {
+        years: lifeExpectancies[d],
+        color: d3.interpolate("#F70202", "#06CF21")(scale(lifeExpectancies[d]))
+    };
+});
 
-var data = [4, 8, 15, 16, 23, 21];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
-
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+/* draw states on id #statesvg */
+uStates.draw("#statesvg", sampleData, tooltipHtml);
